13bb8eb0d6dc74c3915af3cf6f2e496a9c1de01a
Da Li Liu <liudali@cn.ibm.com>
2024-03-27 14:47:17 +0800

    providers: create libvirt-plugin as an example - create libvirt plugin to show how to use provider via runtime plugin - can't use `/run/xxx` as the default plugins folder - keep require packages verions same in different go mods
    Signed-off-by: Da Li Liu <liudali@cn.ibm.com>

---
+++ src/cloud-providers-plugins/libvirt-plugin/manager.go
@@ -0,0 +1,38 @@
+//go:build cgo
+
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"flag"
+
+	providers "github.com/confidential-containers/cloud-api-adaptor/src/cloud-providers"
+	"github.com/confidential-containers/cloud-api-adaptor/src/cloud-providers/libvirt"
+)
+
+type Manager struct {
+	libvirtManager *libvirt.Manager
+}
+
+func InitCloud() {
+	libvirtManager := &libvirt.Manager{}
+	manager := &Manager{
+		libvirtManager: libvirtManager,
+	}
+	providers.AddCloudProvider("libvirt", manager)
+}
+
+func (m *Manager) ParseCmd(flags *flag.FlagSet) {
+	m.libvirtManager.ParseCmd(flags)
+}
+
+func (m *Manager) LoadEnv() {
+
+	m.libvirtManager.LoadEnv()
+}
+
+func (m *Manager) NewProvider() (providers.Provider, error) {
+	return NewProvider(m.libvirtManager.GetConfig())
+}
---
+++ src/cloud-providers-plugins/libvirt-plugin/provider.go
@@ -0,0 +1,72 @@
+//go:build cgo
+
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	providers "github.com/confidential-containers/cloud-api-adaptor/src/cloud-providers"
+	"github.com/confidential-containers/cloud-api-adaptor/src/cloud-providers/libvirt"
+	"github.com/confidential-containers/cloud-api-adaptor/src/cloud-providers/util/cloudinit"
+)
+
+var logger = log.New(log.Writer(), "[adaptor/cloud/libvirt-plugin] ", log.LstdFlags|log.Lmsgprefix)
+
+type libvirtext struct {
+	libvirtProvider providers.Provider
+	serviceConfig   *libvirt.Config
+}
+
+func NewProvider(config *libvirt.Config) (providers.Provider, error) {
+
+	libvirtProvider, err := libvirt.NewProvider(config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	provider := &libvirtext{
+		libvirtProvider: libvirtProvider,
+		serviceConfig:   config,
+	}
+
+	return provider, nil
+}
+
+func (p *libvirtext) CreateInstance(ctx context.Context, podName, sandboxID string, cloudConfig cloudinit.CloudConfigGenerator, spec providers.InstanceTypeSpec) (*providers.Instance, error) {
+	cloudInitCloudConfigData, ok := cloudConfig.(*cloudinit.CloudConfig)
+	// Debug print cloudInitCloudConfigData
+	if !ok {
+		return nil, fmt.Errorf("User Data generator did not use the cloud-init Cloud Config data format")
+	}
+	userData, err := cloudInitCloudConfigData.Generate()
+	if err != nil {
+		return nil, err
+	}
+	logger.Printf("===CreateInstance: userData from libvirt-plugin: %s", userData)
+
+	return p.libvirtProvider.CreateInstance(ctx, podName, sandboxID, cloudConfig, spec)
+}
+
+func (p *libvirtext) DeleteInstance(ctx context.Context, instanceID string) error {
+	return p.libvirtProvider.DeleteInstance(ctx, instanceID)
+}
+
+func (p *libvirtext) Teardown() error {
+	return nil
+}
+
+func (p *libvirtext) ConfigVerifier() error {
+	// Debug print p.serviceConfig
+	logger.Printf("===p.serviceConfig from libvirt-plugin: %v", p.serviceConfig)
+	VolName := p.serviceConfig.VolName
+	if len(VolName) == 0 {
+		return fmt.Errorf("VolName is empty")
+	}
+	return nil
+}
